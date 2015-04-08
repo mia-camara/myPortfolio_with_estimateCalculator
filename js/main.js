d3ebc62629a9d89a8e8b8d72388bf3da0f1695ae
“mia” <mia_dacamara@Mias-MacBook-Pro.local>
2015-04-09 05:09:16 +0800

    Tweaks

--- js/main.js
+++ js/main.js
@@ -26,43 +26,21 @@ var result = 0;
 		$('.sumPrice').text("$" + result + ".00");
 	});
 
-	$(document).on('click','#submit-form',function() {
-		alert('Thank You! I will be in touch soon.')
-	});
-
-	$( "#banner" ).mouseover(function() {
-	  $( ".image" ).animate({
-	    width: [ "toggle", "spin" ],
-	    height: [ "toggle", "spin" ],
-	    opacity: "toggle"
-	  }, 5000, "linear", function() {
-	    $( this ).after();
-	  });
-	});
-
-	function getRadioItem () {
-		chosen = ""
-		length = document.radio.length
-			for (var i = 0; i < length; i++) {
-				if (document.radio[i].checked) {
-					chosen = document.radio[i].value
-				}
-			}		if (chosen == "") {
-				alert ("No Location Chosen")
-			}
-			else {
-				alert(chosen);
-			}
-	}
-
-	getRadioItem(chosen);
-	chosen = document.radio[i].value
-
 });
 
-	// $('#submit-form').onClick function() {
-	// 	alert('Thank You! I will be in touch very soon.');
+	// function submit-form(e) {
+	// alert('Thank You!');
 	// }
+
+	// $( "#banner" ).mouseover(function() {
+	//   $( ".image" ).animate({
+	//     width: [ "toggle", "spin" ],
+	//     height: [ "toggle", "spin" ],
+	//     opacity: "toggle"
+	//   }, 5000, "linear", function() {
+	//     $( this ).after();
+	//   });
+	// });
 
 
 	// $(document).on('click','.answer',function() {
@@ -79,13 +57,3 @@ var result = 0;
 	// 	$($('.sumPrice')[i]).text("$" + result + ".00");
 	// }
 
-	// $("#img").rotate({ 
- //   bind: 
- //     { 
- //        mouseover : function() { 
- //            $(this).rotate({animateTo:180})
- //        },
- //        mouseout : function() { 
- //            $(this).rotate({animateTo:0})
- //        }
- //     } 
